2eb758d56bcc529902fa6ec3445a75bbb647f278
Amrita Dutta <amrita.dutta1497@gmail.com>
2024-03-23 20:28:19 -0400

    Added short answers

--- nfl.sql
+++ nfl.sql
@@ -27,16 +27,45 @@ SELECT name FROM teams;
 SELECT stadium, head_coach FROM teams 
 WHERE conference = 'NFC';
 
+--Answer
+--            stadium	                    head_coach
+-- 1	AT&T Stadium	                    Jason Garrett
+-- 2	MetLife Stadium	                    Tom Coughlin
+-- 3	Lincoln Financial Field	            Chip Kelly
+-- 4	FedExField	                        Jay Gruden
+-- 5	Soldier Field	                    Marc Trestman
+-- 6	Ford Field	                        Jim Caldwell
+-- 7	Lambeau Field           	        Mike McCarthy
+-- 8	TCF Bank Stadium	                Mike Zimmer
+-- 9	Georgia Dome	                    Mike Smith
+-- 10	Bank of America Stadium	            Ron Rivera
+-- 11	Mercedes-Benz Superdome	            Sean Payton
+-- 12	Raymond James Stadium	            Lovie Smith
+-- 13	University of Phoenix Stadium	    Bruce Arians
+-- 14	Edward Jones Dome	                Jeff Fisher
+-- 15	Levis Stadium	                    Jim Harbaugh
+-- 16	CenturyLink Field	                Pete Carroll
+
 
 -- List the head coaches of the AFC South
 SELECT head_coach FROM teams 
 WHERE conference = 'NFC'
 AND division = 'South';
 
+--Answer
+--       head_coach
+-- 1	Mike Smith
+-- 2	Ron Rivera
+-- 3	Sean Payton
+-- 4	Lovie Smith
+
 
 -- The total number of players in the NFL
 SELECT count(*)
 FROM players;
+
+--Answer
+-- count - 1532
 
 
 -- The team names and head coaches of the NFC North and AFC East
@@ -44,6 +73,17 @@ SELECT name, head_coach
 FROM teams
 WHERE (conference = 'NFC' AND division = 'North')
 OR (conference = 'AFC' AND division = 'East');
+
+-- Answer
+--             name	                head_coach
+-- 1	Buffalo Bills	        Doug Marrone
+-- 2	Miami Dolphins	        Joe Philbin
+-- 3	New England Patriots	Bill Belichick
+-- 4	New York Jets	        Rex Ryan
+-- 5	Chicago Bears	        Marc Trestman
+-- 6	Detroit Lions	        Jim Caldwell
+-- 7	Green Bay Packers	    Mike McCarthy
+-- 8	Minnesota Vikings	    Mike Zimmer 
 
 
 -- The 50 players with the highest salaries
@@ -57,6 +97,9 @@ LIMIT 50;
 SELECT avg(salary)
 FROM players;
 
+--Answer
+--avg - 1579692.539817232376
+
 
 -- The names and positions of players with a salary above 10_000_000
 SELECT name, position
@@ -65,10 +108,13 @@ WHERE salary > 10000000;
 
 
 -- The player with the highest salary in the NFL
-SELECT *
+SELECT name
 FROM players
 ORDER BY salary desc
 LIMIT 1;
+
+--Answer
+--name - Peyton Manning
 
 
 -- The name and position of the first 100 players with the lowest salaries
@@ -83,6 +129,9 @@ SELECT avg(salary)
 FROM players
 WHERE position = 'DE';
 
+--Answer
+--avg - 2161326.377049180328
+
 
 -- The total salary of all players on the New York Giants
 SELECT sum(salary)
@@ -90,6 +139,9 @@ FROM players as p
 JOIN teams t 
 ON p.team_id = t.id
 WHERE t.name = 'New York Giants';
+
+--Answer
+-- sum -  74179466
 
 -- The player with the lowest salary on the Green Bay Packers
 SELECT *
@@ -99,3 +151,6 @@ ON p.team_id = t.id
 WHERE t.name = 'Green Bay Packers'
 ORDER BY salary ASC
 LIMIT 1;
+
+--Answer
+-- name - Shaky Smithson
